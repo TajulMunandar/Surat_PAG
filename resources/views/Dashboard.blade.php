5fe4685ec0e24dbde77d56b5e417b674b4a2d678
MuhammadSadiqin <msadiqin46@gmail.com>
2024-06-14 23:48:02 +0700

    Template Web

---
+++ resources/views/Dashboard.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.index')
+@section('content')
+    <div class="container-fluid">
+        <!--  Row 1 -->
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <!-- Surat 1 -->
+                <div class="card overflow-hidden">
+                    <div class="card-body p-4">
+                        <h5 class="card-title mb-9 fw-semibold">
+                            Surat 1
+                        </h5>
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <div class="text-end">
+                                    <p class="text-muted fs-6 mb-0">Jumlah Surat</p>
+                                    <h4 class="fw-semibold">1</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 mb-4">
+                <!-- Surat 1 -->
+                <div class="card overflow-hidden">
+                    <div class="card-body p-4">
+                        <h5 class="card-title mb-9 fw-semibold">
+                            Surat 2
+                        </h5>
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <div class="text-end">
+                                    <p class="text-muted fs-6 mb-0">Jumlah Surat</p>
+                                    <h4 class="fw-semibold">1</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <!-- Surat 1 -->
+                <div class="card overflow-hidden">
+                    <div class="card-body p-4">
+                        <h5 class="card-title mb-9 fw-semibold">
+                            Surat 3
+                        </h5>
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <div class="text-end">
+                                    <p class="text-muted fs-6 mb-0">Jumlah Surat</p>
+                                    <h4 class="fw-semibold">1</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/layouts/index', function () {
+    return view('layouts.index');
+})->name('layouts.index');
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
