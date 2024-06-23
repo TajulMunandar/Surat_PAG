795c0dea4c1443556933390bebdcc39c4879c7a6
MuhammadSadiqin <msadiqin46@gmail.com>
2024-06-23 18:03:03 +0700

    peminjaman PDF template

--- resources/views/template/peminjaman.blade.php
+++ resources/views/template/peminjaman.blade.php
@@ -1,126 +1,260 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Rekap Daftar Hadir</title>
+    <title>Dokumen Baru</title>
     <style>
-        .header-table {
-            font-weight: 600;
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 0;
+        }
+
+        .table th,
+        .table td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
         }
 
         .text-center {
             text-align: center;
         }
 
-        hr {
-            border-top: 2px solid #000;
+        .text-bold {
+            font-weight: bold;
         }
 
-        .header-right {
-            text-align: right;
-        }
-
-        .header-right img {
-            vertical-align: middle;
-        }
-
-        .header-right div {
-            display: inline-block;
-            vertical-align: middle;
-            text-align: left;
-            margin-left: 10px;
-        }
-
-        .header-right .logo-text {
-            font-weight: 600;
-            margin-left: 20px;
-        }
-
-        .header-right .logo-text span {
-            display: block;
-        }
-
-        .header-right .logo-text span:first-child {
-            font-size: 14px;
-        }
-
-        .header-right .logo-text span:last-child {
-            color: red;
-            font-size: 10px;
-        }
-
-
-        .flex-container div {
-            width: 50%;
-        }
-
-        .flex-container table {
-            width: 100%;
-        }
-
-        .info-table {
-            margin-right: 20px;
-        }
-
-        td {
-            padding: 8px 0;
-        }
-
-        .table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .table th,
-        .table td {
-            padding: 2px;
-            border: 1px solid black;
-        }
-
-        .table th p,
-        .table td p {
+        .header {
             margin: 0;
-        }
-
-        .td1 {
-            text-align: justify;
+            padding: 0;
         }
     </style>
 </head>
 
-<body style="padding: 20px;">
-    <table class="table" style="width: 100%;">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th style="text-align: center; padding:20px">
-                    <p style="margin: 0;">Langkah Kerja</p>
-                    <p style="margin: 0; color: red; font-weight: 300">(Di isi paling kurang 5 langkah kerja)</p>
-                </th>
-                <th style="text-align: center">
-                    <p style="margin: 0">Bahaya Kecelakaan Potensial</p>
-                </th>
-                <th style="text-align: center">
-                    <p style="margin: 0">Tindakan Pencegahan Kecelakaan</p>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
+<body>
+    <div class="header">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th style="width: 20%; text-align: center; padding: 20px;">
+                        <p style="margin: 0;">#CSR</p>
+                    </th>
+                    <th style="width: 60%; text-align: center;">
+                        <p style="margin: 0;"><span style="border-bottom: 1px solid black;">Please Send Complete Data To
+                                GS & IT</span></p>
+                        <p style="margin: 0;">Silahkan Dikirim ke Fungsi GS&IT Apabila Sudah diisi dan Disetujui</p>
+                    </th>
+                    <th style="width: 20%; text-align: center; padding: 20px;">
+                        <p style="margin: 0;">Received Date</p>
+                    </th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    <div style="margin-top: 10px; width: 100%;">
+        <table class="info-table" style="width: 100%; height: 10%;">
+            <thead style="margin-bottom: 0;">
+                <tr>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 118px">Nama Pemakai</span> :
+                        <span
+                            style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asdas</span>
+                    </td>
+                    <td style="vertical-align: top; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 115px">Nama Divisi</span> :
+                        <span
+                            style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asda</span>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 143px">No Pegawai</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 125px">Unit Kerja</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 127px">Tempat Lahir</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 113px">No Telphone</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 125px">Tanggal Lahir</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 117px">Tanggal Masuk</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; text-align: justify; width: 32%; padding-right: 10%;">
+                        <span style="font-weight: 600; margin-right: 170px">Jabatan</span> :
+                        <span style="border-bottom: 1px solid black; line-height: 40px; padding-bottom: 10px">asd</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div style="margin-top: 20px; margin-left: 40px">
+        <table border style="width: 100%">
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top; width: 50%" rowspan="10">TYPE OF SERVICE</td>
+                    <td colspan="2">Software / application</td>
+                </tr>
+                <tr style="height: 50px;"></tr>
+                <tr style="padding-top: 50px;">
+                    <td>
+                    </td>
+                    <td style="width: 50% ">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">SAP GUI</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 5%"></td>
+                    <td colspan="3">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">New / Modification
+                                Program/System</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 5%"></td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">Authorization/Tcode</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 5%"></td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">PC Standard</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 5%"></td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">Other</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4">Hardware</td>
+                </tr>
+                <tr>
+                    <td style="width: 5% padding-right: px;"></td>
+                    <td style="">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+                            <label class="form-check-label" for="flexCheckDefault1">Server</label>
+                        </div>
+                    </td>
+                    <td style="width: 30%">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2">
+                            <label class="form-check-label" for="flexCheckDefault2">PC</label>
+                        </div>
+                    </td>
+                    <td style="width: 30%">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault3">
+                            <label class="form-check-label" for="flexCheckDefault3">Laptop</label>
+                        </div>
+                    </td>
+                    <td style="width: 30%">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault4">
+                            <label class="form-check-label" for="flexCheckDefault4">Printer/Scanner</label>
+                        </div>
+                    </td>
+                </tr>
 
-            <tr style="text-align: center">
-                <td style=" padding:10px"></td>
-                <td>asdasd</td>
-                <td>dasda</td>
-                <td>dasdas</td>
-            </tr>
+                <tr>
+                    <td colspan="2">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">Data Komunikasi</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td colspan="5">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">User ID - Lampirkan Form-124
+                                Permintaan User ID</label>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div style="margin-top: 20px; margin-left: 40px">
+        <table style="width: 100%;">
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top; max-width: 50%; padding-right: 13%; word-wrap: break-word;">
+                        <span style="font-weight: 600; display: inline-block; width: 50%; max-width: 50%;">Description
+                            Of Services</span> :
+                        <span
+                            style="display: inline-block; max-width: 100%; word-wrap: break-word; overflow-wrap: break-word; border-bottom: 1px solid black;">dasdasdasdasdsasaddsasdassaddsadasdasdadasdasdsadasdasdasdsasdasdasdasdasdasdasdsadasdasdasdasdasdasdasdasdasdasdasdasdasdsadassa</span>
+                    </td>
 
-        </tbody>
-    </table>
-
-
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div style="margin-top: 20px; margin-left: 40px">
+        <table border style="width: 100%">
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top; text-align:justify; width: 32%; padding-right:10%" rowspan="10">
+                        JUSTIFICATION</td>
+                    <td style="text-align:justify; width: 60%;">
+                        :
+                        <span style="border-bottom: 1px solid black;">sasad</span>
+                    </td>
+                </tr>
+                <tr style="height: 50px;"></tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
